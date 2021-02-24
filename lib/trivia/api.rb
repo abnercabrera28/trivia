ef04d1ff9225dc381e315832fa2fb953a894337f
Abner <abnercabrera28@gmail.com>
2021-02-24 03:46:00 -0500

    changed URL to boolean questions

--- lib/trivia/api.rb
+++ lib/trivia/api.rb
@@ -1,6 +1,6 @@
 class Trivia::API
 
-    URL = "https://opentdb.com/api.php?amount=50"
+    URL = "https://opentdb.com/api.php?amount=50&type=boolean"
 
     def get_trivia
         hash = HTTParty.get(URL)
