1e25e9e8d0da446d8c26ba2c7a6e12d7756eba33
Abner <abnercabrera28@gmail.com>
2021-02-24 03:26:12 -0500

    added get_trivia method

--- lib/trivia/api.rb
+++ lib/trivia/api.rb
@@ -1,3 +1,11 @@
 class Trivia::API
 
+    URL = "https://opentdb.com/api.php?amount=50"
+
+    def get_trivia
+        hash = HTTParty.get(URL)
+        hash["results"]
+        binding.pry
+    end
+
 end
