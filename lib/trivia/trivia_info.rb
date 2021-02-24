ed0d8aaa4aa860fac82e0d21a666452b3dd40b8e
Abner <abnercabrera28@gmail.com>
2021-02-24 15:37:09 -0500

    set up the initialization

--- lib/trivia/trivia_info.rb
+++ lib/trivia/trivia_info.rb
@@ -1,3 +1,17 @@
 class Trivia::Info
 
+    @@all = []
+
+    def initialize(hash)
+        hash.each do |key, value| 
+          self.class.attr_accessor(key)
+          self.send("#{key}=", value)
+        end
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
