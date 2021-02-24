c15cfcd5229d89b3eb77abe3bdedb481bd033ef2
Abner <abnercabrera28@gmail.com>
2021-02-24 15:40:35 -0500

    redefined run method

--- lib/trivia/cli.rb
+++ lib/trivia/cli.rb
@@ -2,10 +2,17 @@ class Trivia::CLI
     
     def run
         cheers
+        Trivia::API
+        start
     end
 
     def cheers
         puts "Welcome to The Greatest Trivia Show!!!"
+    end
+
+    def start
+        puts "Would you like to test your trivia knowledge?"
+        question
     end
 
 end
