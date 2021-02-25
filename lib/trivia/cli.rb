cce4616b2357148f03f9ab04b3d8115c96f2ff9b
Abner <abnercabrera28@gmail.com>
2021-02-25 13:33:18 -0500

    defined input method

--- lib/trivia/cli.rb
+++ lib/trivia/cli.rb
@@ -2,8 +2,10 @@ class Trivia::CLI
     
     def run
         cheers
-        Trivia::API
+        Trivia::API.new.get_trivia
         start
+        question_list
+    
     end
 
     def cheers
@@ -12,7 +14,25 @@ class Trivia::CLI
 
     def start
         puts "Would you like to test your trivia knowledge?"
-        question
+
+        
+
+
+    end
+
+    def question_list
+        empty = []
+        empty << Trivia::Info.all
+        empty
+        binding.pry
+    end
+
+    def question
+        #takes one question from list of questions
+    end
+
+    def user_input
+        gets.strip
     end
 
 end
